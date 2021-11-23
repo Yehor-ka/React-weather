0cde488ceabf3ed2c30a9cbd5621d03a5e8ff035
Yehor-ka <kalolg0109@gmail.com>
2021-11-23 17:37:21 +0200

    First commit react-weather-app

---
+++ src/pages/MonthStatistics/components/MonthStatistics.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const MonthStatistics = (props: Props) => {
+    return (
+        <div>
+            Month Statistics
+        </div>
+    )
+}
+
